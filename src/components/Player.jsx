13c5dd232497b41df8f85f07800e42440b6ffe01
PT-HUANG <qwe1593574268@gmail.com>
2025-02-13 17:45:28 +0800

    add: spotify iframe embed container

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,7 +1,10 @@
 function Player() {
   return (
-    <div className="player_container">Spotify播放器</div>
+    <div className="player_container">
+      <div className="player_title">正在播放</div>
+      <hr className="player_hr" />
+    </div>
   );
 }
 
-export default Player
+export default Player;
