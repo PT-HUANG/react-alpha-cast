fe054b82f6aeba94b194385d7e3eb0b626d92687
PT-HUANG <qwe1593574268@gmail.com>
2025-03-04 00:49:49 +0800

    fix: gh-pages config

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.scss";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, BrowserRouter, Routes, Route } from "react-router-dom";
 import { Welcome, Login, Home } from "./pages";
 import { AuthProvider } from "./context/AuthContext";
 import { UserProvider } from "./Context/UserContext";
@@ -7,21 +7,23 @@ import { PlayerProvider } from "./Context/PlayerContext";
 
 function App() {
   return (
-    <div className="app">
-      <AuthProvider>
-        <UserProvider>
-          <PlayerProvider>
-            <BrowserRouter>
-              <Routes>
-                <Route path="/" element={<Welcome />} />
-                <Route path="login" element={<Login />} />
-                <Route path="home" element={<Home />} />
-              </Routes>
-            </BrowserRouter>
-          </PlayerProvider>
-        </UserProvider>
-      </AuthProvider>
-    </div>
+    <HashRouter>
+      <div className="app">
+        <AuthProvider>
+          <UserProvider>
+            <PlayerProvider>
+              <BrowserRouter>
+                <Routes>
+                  <Route path="/" element={<Welcome />} />
+                  <Route path="login" element={<Login />} />
+                  <Route path="home" element={<Home />} />
+                </Routes>
+              </BrowserRouter>
+            </PlayerProvider>
+          </UserProvider>
+        </AuthProvider>
+      </div>
+    </HashRouter>
   );
 }
 
--- vite.config.js
+++ vite.config.js
@@ -10,5 +10,5 @@ export default defineConfig({
   build: {
     target: "esnext",
   },
-  base: "/react-alpha-cast",
+  base: "/react-alpha-cast/",
 });
