6bdf6147716d3209dc0e6fddb6efcdbf12d8da49
PT-HUANG <qwe1593574268@gmail.com>
2025-03-04 00:41:20 +0800

    fix: gh-pages config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { ghPages } from "vite-plugin-gh-pages";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), ghPages()],
+  plugins: [react()],
   server: {
     port: 3000,
   },
